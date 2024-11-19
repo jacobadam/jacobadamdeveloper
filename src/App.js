2f2678f919bc60635cf51d1c05a99770fb41a1b2
jacobadam <jacobanevitt@gmail.com>
2024-11-19 09:18:00 -0800

    install tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Jacob Adam Developer</div>;
+export default function App() {
+  return <h1 className="text-3xl font-bold">Hello world!</h1>;
 }
-
-export default App;
