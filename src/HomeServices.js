6e58aa915a4416aac6923c118865bc4da12e627c
jacobadam <jacobanevitt@gmail.com>
2024-11-20 12:19:57 -0800

    update services with grid

--- src/HomeServices.js
+++ src/HomeServices.js
@@ -13,7 +13,7 @@ export default function HomeServices() {
             doloremque laudantium, totam rem aperiam, eaque ipsa quae.
           </p>
         </div>
-        <div className="flex justify-center items-center gap-x-5 gap-y-8 lg:gap-y-0 flex-wrap md:flex-wrap lg:flex-nowrap lg:flex-row lg:justify-between lg:gap-x-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {[
             {
               title: "Custom Front-End Website",
@@ -40,10 +40,7 @@ export default function HomeServices() {
               svgPath: "/seo2.svg",
             },
           ].map((feature, index) => (
-            <div
-              key={index}
-              className="relative w-full text-center max-md:max-w-sm max-md:mx-auto group md:w-2/5 lg:w-1/4 min-h-[300px]"
-            >
+            <div key={index} className="relative w-full text-center p-6">
               <div className="rounded-lg flex justify-center items-center mb-5 w-20 h-20 mx-auto cursor-pointer transition-all duration-500">
                 <img
                   src={feature.svgPath}
