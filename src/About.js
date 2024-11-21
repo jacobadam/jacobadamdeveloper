6492792980811c40e716903d806b5d0fbb1c985b
jacobadam <jacobanevitt@gmail.com>
2024-11-21 10:47:59 -0800

    add about me

--- src/About.js
+++ src/About.js
@@ -1,5 +1,37 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <section className="py-24 relative">
+      <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+        <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
+          <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
+            <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
+              <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
+                Hello, I’m Jacob.
+              </h2>
+              <h3>Freelance Web Developer</h3>
+              <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+                doloremque laudantium, totam rem aperiam, eaque ipsa quaeSed ut
+                perspiciatis unde omnis iste natus voluptatem accusantium
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae
+              </p>
+            </div>
+            <a
+              href="/contact"
+              className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+            >
+              Contact me
+            </a>
+          </div>
+          <img
+            className="lg:mx-0 mx-auto h-full rounded-3xl object-cover"
+            src="/jacobadam.webp"
+            alt="Jacob Adam"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
