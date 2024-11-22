6ecfbcdcfcb2df0f6d086f73829fc86db9b9ffe9
jacobadam <jacobanevitt@gmail.com>
2024-11-22 12:31:53 -0800

    update styling and background

--- src/Contact.js
+++ src/Contact.js
@@ -2,105 +2,117 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <section className="py-24">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
-          <div>
-            <h2 className="text-gray-900 text-4xl font-semibold font-manrope leading-snug pb-5">
-              Contact Us
-            </h2>
-            <p className="text-gray-600 text-lg font-normal pb-6">
-              This is your gateway to connect with us directly. Whether you have
-              inquiries, feedback, or simply want to say hello.
-            </p>
-            <div className="py-6 border-y border-indigo-100">
-              <div className="group flex gap-4">
-                <div className="rounded-lg transition-all duration-700 ease-in-out p-3">
-                  <img
-                    src="/email2.svg"
-                    alt="Email Icon"
-                    className="w-6 h-6 object-contain"
-                  />
+    <div className="relative">
+      <div className="absolute inset-0 blur-xl bg-slate-200"></div>
+
+      <section className="py-24 relative">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
+            <div>
+              <h2 className="text-gray-900 text-4xl font-semibold font-manrope leading-snug pb-5">
+                Contact Us
+              </h2>
+              <p className="text-gray-600 text-lg font-normal pb-6">
+                This is your gateway to connect with us directly. Whether you
+                have inquiries, feedback, or simply want to say hello.
+              </p>
+              <div className="py-6 border-y border-black">
+                <div className="group flex gap-4">
+                  <div className="rounded-lg transition-all duration-700 ease-in-out p-3">
+                    <img
+                      src="/email2.svg"
+                      alt="Email Icon"
+                      className="w-6 h-6 object-contain"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <h6 className="text-gray-900 text-sm font-medium">Email</h6>
+                    <p className="text-gray-700 text-xs font-normal">
+                      jacobadamdeveloper@gmail.com
+                    </p>
+                  </div>
                 </div>
-                <div className="flex flex-col gap-2">
-                  <h6 className="text-gray-900 text-sm font-medium">Email</h6>
-                  <p className="text-gray-700 text-xs font-normal">
-                    jacobadamdeveloper@gmail.com
-                  </p>
+                <div className="group flex gap-4 my-8">
+                  <div className="rounded-lg transition-all duration-700 ease-in-out p-3">
+                    <img
+                      src="/phone2.svg"
+                      alt="Phone Icon"
+                      className="w-6 h-6 object-contain"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <h6 className="text-gray-900 text-sm font-medium">
+                      Contact
+                    </h6>
+                    <p className="text-gray-700 text-xs font-normal">
+                      (+1) 236-885-4130
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="group flex gap-4 my-8">
-                <div className="rounded-lg transition-all duration-700 ease-in-out p-3">
-                  <img
-                    src="/phone2.svg"
-                    alt="Phone Icon"
-                    className="w-6 h-6 object-contain"
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <h6 className="text-gray-900 text-sm font-medium">Contact</h6>
-                  <p className="text-gray-700 text-xs font-normal">
-                    (+1) 236-885-4130
-                  </p>
+              <div className="flex gap-6 mt-6">
+                <div className="flex space-x-10 justify-center items-center mb-10">
+                  <a
+                    href="https://www.linkedin.com/in/jacobnevitt/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block text-gray-900 transition-all duration-500 hover:text-indigo-600"
+                  >
+                    <img
+                      src="/linkedin.svg"
+                      alt="LinkedIn Logo"
+                      className="w-8 h-8"
+                    />
+                  </a>
                 </div>
               </div>
             </div>
-            <div className="flex gap-6 mt-6">
-              <div class="flex space-x-10 justify-center items-center mb-10">
-                <img
-                  href="https://www.linkedin.com/in/jacobnevitt/"
-                  class="block  text-gray-900 transition-all duration-500 hover:text-indigo-600 w-8 h-8"
-                  src="/linkedin.svg"
-                  alt="Linkedin Logo"
-                />
-              </div>
+            <div>
+              <form action="" method="POST" className="space-y-4">
+                <div>
+                  <label htmlFor="name" className="text-gray-900">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full border rounded-md px-4 py-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email" className="text-gray-900">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full border rounded-md px-4 py-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="message" className="text-gray-900">
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows="4"
+                    className="w-full border rounded-md px-4 py-2"
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-indigo-600 hover:bg-indigo-700 text-white py-2 rounded-md"
+                >
+                  Submit
+                </button>
+              </form>
             </div>
-          </div>
-          <div>
-            <form action="" method="POST" className="space-y-4">
-              <div>
-                <label htmlFor="name" className="text-gray-900">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full border rounded-md px-4 py-2"
-                />
-              </div>
-              <div>
-                <label htmlFor="email" className="text-gray-900">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full border rounded-md px-4 py-2"
-                />
-              </div>
-              <div>
-                <label htmlFor="message" className="text-gray-900">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows="4"
-                  className="w-full border rounded-md px-4 py-2"
-                ></textarea>
-              </div>
-              <button
-                type="submit"
-                className="w-full bg-indigo-600 hover:bg-indigo-700 text-white py-2 rounded-md"
-              >
-                Submit
-              </button>
-            </form>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
