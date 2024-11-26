fe48c0dc389fea5d57aed045b23b6a28af443eca
jacobadam <jacobanevitt@gmail.com>
2024-11-26 11:35:39 -0800

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const tailwindConfig = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      xxxs: "378px",
+      xxs: "424px",
+      xs: "540px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {},
+    },
   },
   plugins: [],
 };
+
+export default tailwindConfig;
