a52e227d8b58de896a9d483fdf3986cd77b5418b
jacobadam <jacobanevitt@gmail.com>
2024-11-27 09:13:18 -0800

    update styling on contact form

--- src/Contact.js
+++ src/Contact.js
@@ -5,10 +5,10 @@ export default function Contact() {
     <div className="relative">
       <div className="absolute inset-0 blur-xl bg-slate-200"></div>
 
-      <section className="py-24 relative">
+      <section className="py-16 relative">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
-            <div>
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 lg:items-start items-center lg:justify-start justify-center">
+            <div className="flex flex-col items-center text-center px-4 sm:px-6">
               <h2 className="text-gray-900 text-4xl font-semibold font-manrope leading-snug pb-5">
                 Contact Us
               </h2>
@@ -16,57 +16,57 @@ export default function Contact() {
                 This is your gateway to connect with us directly. Whether you
                 have inquiries, feedback, or simply want to say hello.
               </p>
-              <div className="py-6 border-y border-black">
-                <div className="group flex gap-4">
-                  <div className="rounded-lg transition-all duration-700 ease-in-out p-3">
+              <div className="py-6 border-y border-stone-300 text-center">
+                <div className="flex items-center gap-4">
+                  <div className="rounded-lg p-3">
                     <img
                       src="/email2.svg"
                       alt="Email Icon"
                       className="w-6 h-6 object-contain"
                     />
                   </div>
-                  <div className="flex flex-col gap-2">
+                  <div className="flex flex-col items-center w-full">
                     <h6 className="text-gray-900 text-sm font-medium">Email</h6>
-                    <p className="text-gray-700 text-xs font-normal">
+                    <p className="text-gray-700 text-xs font-normal text-center">
                       jacobadamdeveloper@gmail.com
                     </p>
                   </div>
                 </div>
-                <div className="group flex gap-4 mt-8">
-                  <div className="rounded-lg transition-all duration-700 ease-in-out p-3">
+                <div className="flex items-center gap-4 mt-8">
+                  <div className="rounded-lg p-3">
                     <img
                       src="/phone2.svg"
                       alt="Phone Icon"
                       className="w-6 h-6 object-contain"
                     />
                   </div>
-                  <div className="flex flex-col gap-2">
+                  <div className="flex flex-col items-center w-full">
                     <h6 className="text-gray-900 text-sm font-medium">
                       Contact
                     </h6>
-                    <p className="text-gray-700 text-xs font-normal">
+                    <p className="text-gray-700 text-xs font-normal text-center">
                       (+1) 236-885-4130
                     </p>
                   </div>
                 </div>
               </div>
-              <div className="flex gap-6 mt-6">
-                <div className="flex space-x-10 justify-center items-center mb-10">
-                  <a
-                    href="https://www.linkedin.com/in/jacobnevitt/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="block text-gray-900 transition-all duration-500 hover:text-indigo-600"
-                  >
-                    <img
-                      src="/linkedin.svg"
-                      alt="LinkedIn Logo"
-                      className="w-8 h-8"
-                    />
-                  </a>
-                </div>
+
+              <div className="flex justify-center items-center mb-10 mt-6">
+                <a
+                  href="https://www.linkedin.com/in/jacobnevitt/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block text-gray-900 transition-all duration-500 hover:text-indigo-600"
+                >
+                  <img
+                    src="/linkedin.svg"
+                    alt="LinkedIn Logo"
+                    className="w-8 h-8"
+                  />
+                </a>
               </div>
             </div>
+
             <div>
               <form action="" method="POST" className="space-y-4">
                 <div>
