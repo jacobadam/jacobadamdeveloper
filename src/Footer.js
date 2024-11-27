ae42b86ce548287dfbdea3567072d00d276b9f06
jacobadam <jacobanevitt@gmail.com>
2024-11-27 13:04:32 -0800

    add gradient to footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,47 +2,26 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="w-full bg-white">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="max-w-3xl mx-auto">
-          {/* <ul class="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-10 mb-10 border-b border-black">
-            <li>
-              <a href="/" class="text-gray-800 hover:text-gray-900">
-                Home
+    <footer className="w-full bg-gradient-to-b from-slate-200 to-white">
+      <div className="w-full">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="max-w-3xl mx-auto">
+            <div className="flex space-x-10 justify-center items-center mb-10 mt-4">
+              <a
+                href="https://www.linkedin.com/in/jacobnevitt/"
+                className="block text-gray-900 transition-all duration-500 hover:text-indigo-600"
+              >
+                <img
+                  className="w-8 h-8"
+                  src="/linkedin.svg"
+                  alt="LinkedIn Logo"
+                />
               </a>
-            </li>
-            <li>
-              <a href="/about:;" class=" text-gray-800 hover:text-gray-900">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/services" class=" text-gray-800 hover:text-gray-900">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="/portfolio" class=" text-gray-800 hover:text-gray-900">
-                Portfolio
-              </a>
-            </li>
-            <li>
-              <a href="/contact" class=" text-gray-800 hover:text-gray-900">
-                Contact
-              </a>
-            </li>
-          </ul> */}
-          <div class="flex space-x-10 justify-center items-center mb-10 mt-4">
-            <img
-              href="https://www.linkedin.com/in/jacobnevitt/"
-              class="block  text-gray-900 transition-all duration-500 hover:text-indigo-600 w-8 h-8"
-              src="/linkedin.svg"
-              alt="Linkedin Logo"
-            />
+            </div>
+            <span className="text-lg text-gray-500 text-center block pb-4">
+              © 2024 Jacob Adam. All Rights Reserved.
+            </span>
           </div>
-          <span class="text-lg text-gray-500 text-center block pb-4">
-            © 2024 Jacob Adam. All Rights Reserved.
-          </span>
         </div>
       </div>
     </footer>
