ead2e76a897d0de7e69a0a4d3ea169459f6626f8
jacobadam <jacobanevitt@gmail.com>
2024-11-27 13:09:11 -0800

    remove divider

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,15 @@
 import HomeContact from "./HomeContact";
-import HomeDivider from "./HomeDivider";
 import HomeHero from "./HomeHero";
 import HomePortfolio from "./HomePortfolio";
 import HomeServices from "./HomeServices";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <div className="absolute inset-0 blur-xl bg-slate-200"></div>
-      <div className="relative">
-        <HomeHero />
-        <HomeDivider />
-        <HomePortfolio />
-        <HomeDivider />
-        <HomeServices />
-        <HomeDivider />
-        <HomeContact />
-      </div>
+    <div>
+      <HomeHero />
+      <HomePortfolio />
+      <HomeServices />
+      <HomeContact />
     </div>
   );
 }
