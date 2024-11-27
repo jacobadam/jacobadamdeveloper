1f2c586556f76637385c70dd827d9a76aaaa7262
jacobadam <jacobanevitt@gmail.com>
2024-11-27 13:10:28 -0800

    delete divider

--- src/HomeDivider.js
+++
@@ -1,8 +0,0 @@
-export default function HomeDivider() {
-  return (
-    <span className="flex items-center pb-16">
-      <span className="h-px flex-1"></span>
-      <span className="h-px flex-1"></span>
-    </span>
-  );
-}
