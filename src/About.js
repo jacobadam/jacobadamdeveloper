7fbad921756ca74fdf473c450ef079f1e4e7c728
jacobadam <jacobanevitt@gmail.com>
2024-12-04 13:28:27 -0800

    update styling and button

--- src/About.js
+++ src/About.js
@@ -2,35 +2,69 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="py-24 bg-slate-200 flex-grow">
+    <section className="py-12 relative bg-slate-200">
       <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
-        <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
-          <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
-            <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
-              <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
-                Hello, I’m Jacob.
-              </h2>
-              <h3>Freelance Web Developer</h3>
-              <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
-                Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
-                doloremque laudantium, totam rem aperiam, eaque ipsa quaeSed ut
-                perspiciatis unde omnis iste natus voluptatem accusantium
-                doloremque laudantium, totam rem aperiam, eaque ipsa quae
-              </p>
-            </div>
-            <a
-              href="/contact"
-              className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-            >
-              Contact me
-            </a>
+        <div className="w-full flex-col justify-start items-start lg:gap-12 gap-10 inline-flex">
+          {/* Header Section */}
+          <div className="w-full flex-col justify-start items-center gap-3 flex">
+            <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
+              Hello,{" "}
+              <span className="text-indigo-600 font-black">I’m Jacob.</span>
+            </h2>
+            <p className="max-w-xl text-center text-gray-500 text-base font-normal leading-relaxed">
+              As a Web Development Services We are Committed to Building Custom
+              Web Solutions that Drive Business Success.
+            </p>
           </div>
-          <div className="flex justify-center items-center">
+
+          {/* Content Section */}
+          <div className="justify-start items-center lg:gap-12 gap-10 grid lg:grid-cols-2 grid-cols-1">
+            {/* Image Section */}
             <img
-              className="rounded-full w-120 h-120 object-cover"
+              className="lg:w-full mx-auto lg:h-full rounded-3xl object-cover"
               src="/jacobadam.png"
               alt="Jacob Adam"
             />
+
+            {/* Text Content Section */}
+            <div className="flex-col justify-center lg:items-start items-center gap-10 inline-flex">
+              <div className="flex-col justify-center items-start gap-3 flex">
+                <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                  At our web development agency, we specialize in crafting
+                  bespoke online solutions tailored to elevate businesses toward
+                  their digital objectives. Committed to continuous evolution,
+                  we remain abreast of cutting-edge web technologies and trends,
+                  ensuring a constant delivery of unparalleled excellence.
+                </p>
+                <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                  Our ethos is rooted in equipping clients with the essential
+                  tools for triumph in the digital realm. Committed to
+                  continuous evolution, we remain abreast of cutting-edge web
+                  technologies and trends, ensuring a constant delivery of
+                  unparalleled excellence. We firmly believe in furnishing every
+                  business with a digital presence that authentically embodies
+                  their brand essence and effectively resonates with their
+                  target demographic.
+                </p>
+              </div>
+
+              {/* Button Section */}
+              <a
+                href="/contact"
+                className="flex items-center justify-center gap-x-1 py-1 xxxs:py-2 px-4 text-white font-medium bg-indigo-600 duration-150 rounded-full"
+                aria-label="Visit services page"
+              >
+                Contact me
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
