32eaf28ecdc00683b9b29d3b23a17bcd698ebe31
jacobadam <jacobanevitt@gmail.com>
2024-12-06 11:45:24 -0800

    re factor nav and add z index

--- src/Nav.js
+++ src/Nav.js
@@ -18,57 +18,59 @@ export default function NavBar() {
     };
   }, []);
 
-  const Brand = () => (
-    <div className="flex items-center justify-between pt-8 md:block">
-      <Link to="/">
-        <img src="/j-icon.svg" width={60} height={25} alt="J logo" />
-      </Link>
-      <div className="md:hidden">
-        <button
-          className="menu-btn text-gray-500 hover:text-gray-800"
-          onClick={() => setState(!state)}
-          aria-label={state ? "Close menu" : "Open menu"}
-          aria-expanded={state ? "true" : "false"}
-          aria-controls="mobile-nav"
-        >
-          {state ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          )}
-        </button>
-      </div>
-    </div>
-  );
-
   return (
     <header>
       <div className={`md:hidden ${state ? "mx-2 pb-5" : "hidden"}`}>
-        <Brand />
+        <div className="flex items-center justify-between pt-8 md:block">
+          <Link to="/">
+            <img
+              className=""
+              src="/j-icon.svg"
+              width={60}
+              height={25}
+              alt="J logo"
+            />
+          </Link>
+          <div className="md:hidden">
+            <button
+              className="menu-btn text-gray-500 hover:text-gray-800"
+              onClick={() => setState(!state)}
+              aria-label={state ? "Close menu" : "Open menu"}
+              aria-expanded={state ? "true" : "false"}
+              aria-controls="mobile-nav"
+            >
+              {state ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
       </div>
       <nav
         className={`pb-5 md:text-sm ${
@@ -80,16 +82,66 @@ export default function NavBar() {
         id="mobile-nav"
         style={{
           background: "linear-gradient(to bottom, white, #e2e8f0)",
+          zIndex: 1000,
         }}
       >
         <div className="items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
-          <Brand />
+          <div className="flex items-center justify-between pt-8 md:block">
+            <Link to="/">
+              <img
+                className=""
+                src="/j-icon.svg"
+                width={60}
+                height={25}
+                alt="J logo"
+              />
+            </Link>
+            <div className="md:hidden">
+              <button
+                className="menu-btn text-gray-500 hover:text-gray-800"
+                onClick={() => setState(!state)}
+                aria-label={state ? "Close menu" : "Open menu"}
+                aria-expanded={state ? "true" : "false"}
+                aria-controls="mobile-nav"
+              >
+                {state ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
           <div
             className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
               state ? "block" : "hidden"
             } `}
           >
-            <ul className="flex-1 justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+            <ul className="flex flex-col justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
               {navigation.map((item, idx) => (
                 <li
                   key={idx}
