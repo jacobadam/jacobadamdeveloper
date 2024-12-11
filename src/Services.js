9a0f59c273a1023c35ea0c01b51a32316c94a89a
jacobadam <jacobanevitt@gmail.com>
2024-12-11 12:55:42 -0800

    update styling and positioning

--- src/Services.js
+++ src/Services.js
@@ -8,17 +8,24 @@ export default function Services() {
   };
 
   return (
-    <section className="pt-4 pb-20 relative bg-slate-200 flex-grow">
+    <section className="pt-4 xs:pt-8 pb-20 relative bg-slate-200 flex-grow">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mb-8 text-center">
-          <h2 className="text-gray-900 text-2xl font-semibold leading-snug pb-5">
-            Services
-          </h2>
+          <div className="flex items-center justify-between gap-5 flex-col mb-14">
+            <h2 className="text-gray-900 text-2xl lg:text-3xl font-semibold leading-snug">
+              Services
+            </h2>
+            <p className="font-normal text-base text-gray-500 max-w-3xl text-center">
+              Quam maecenas lacus cursus enim sit rhoncus eu pellentesque arcu.
+              Vehicula diam ornare habitant pellentesque. Molestie convallis
+              aliquet amet in tristique ornare commodo.
+            </p>
+          </div>
         </div>
         <div className="mb-4">
           <div className="tabs">
             <div className="flex w-full items-center justify-center">
-              <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:grid-cols-3 lg:gap-8 overflow-x-auto mb-9 lg:mb-16">
+              <ul className="grid grid-cols-1 sm:grid-cols-2 sm:gap-4 lg:grid-cols-3 lg:gap-8 overflow-x-auto sm:mb-9 lg:mb-16">
                 {[
                   {
                     id: "tabs-with-pill-1",
@@ -32,7 +39,7 @@ export default function Services() {
                     id: "tabs-with-pill-3",
                     label: (
                       <>
-                        Custom Built Website and
+                        Custom Built Website &
                         <br />
                         Full Content Package
                       </>
@@ -42,7 +49,7 @@ export default function Services() {
                   <li key={tab.id} className="w-full">
                     <button
                       onClick={() => handleTabClick(tab.id)}
-                      className={`group flex items-center justify-center text-center gap-4 py-5 lg:py-9 px-6 bg-slate-200 rounded-2xl text-base font-medium text-gray-900 transition-all duration-500 w-full min-h-[120px] hover:text-white hover:bg-gradient-to-tr from-indigo-600 to-violet-600 ${
+                      className={`group flex items-center justify-center text-center gap-4 py-5 lg:py-9 px-6 bg-slate-200 rounded-2xl text-base font-medium text-gray-900 transition-all duration-500 w-full sm:min-h-[120px] hover:text-white hover:bg-gradient-to-tr from-indigo-600 to-violet-600 ${
                         activeTab === tab.id
                           ? "bg-gradient-to-tr text-white"
                           : ""
@@ -64,9 +71,9 @@ export default function Services() {
                   style={{ display: "block" }}
                 >
                   <section className="">
-                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-6 flex justify-center">
+                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-6 flex justify-center text-center lg:text-left">
                       <div className="max-w-screen-lg text-black sm:text-lg">
-                        <h2 className="mb-4 text-4xl tracking-tight font-bold text-gray-900">
+                        <h2 className="mb-4 text-3xl tracking-tight font-bold text-gray-900">
                           Performance Optimization
                         </h2>
                         <p className="mb-4 font-light">
@@ -96,9 +103,9 @@ export default function Services() {
                   style={{ display: "block" }}
                 >
                   <section className="">
-                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-6 flex justify-center">
+                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-6 flex justify-center text-center lg:text-left">
                       <div className="max-w-screen-lg text-black sm:text-lg">
-                        <h2 className="mb-4 text-4xl tracking-tight font-bold text-gray-900">
+                        <h2 className="mb-4 text-3xl tracking-tight font-bold text-gray-900">
                           Custom Built Website
                         </h2>
                         <p className="mb-4 font-light">
@@ -128,9 +135,9 @@ export default function Services() {
                   style={{ display: "block" }}
                 >
                   <section className="">
-                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-6 flex justify-center">
+                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-6 flex justify-center text-center lg:text-left">
                       <div className="max-w-screen-lg text-black sm:text-lg">
-                        <h2 className="mb-4 text-4xl tracking-tight font-bold text-gray-900">
+                        <h2 className="mb-4 text-3xl tracking-tight font-bold text-gray-900">
                           Custom Built Website and Full Content Package
                         </h2>
                         <p className="mb-4 font-light">
