082dc475a1b13118ef296ffa6be594d81b81f86d
jacobadam <jacobanevitt@gmail.com>
2024-12-17 15:31:55 -0800

    update dark mode styles

--- src/Footer.js
+++ src/Footer.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-gradient-to-b from-slate-200 to-white">
+    <footer className="w-full bg-gradient-to-b from-slate-200 to-white dark:from-black dark:to-black">
       <div className="w-full">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="max-w-3xl mx-auto">
             <div className="flex space-x-10 justify-center items-center mb-10 mt-4">
               <a
                 href="https://www.linkedin.com/in/jacobnevitt/"
-                className="block text-gray-900 transition-all duration-500 hover:text-indigo-600"
+                className="block text-gray-900 dark:invert transition-all duration-500 hover:text-indigo-600"
               >
                 <img
                   className="w-8 h-8"
