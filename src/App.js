4c6bcb4dbe335894c9340f81b11d5af78909af70
jacobadam <jacobanevitt@gmail.com>
2024-12-19 13:11:33 -0800

    add 404 page and catch all route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import About from "./About";
-import Services from "./Services";
-import Portfolio from "./Portfolio";
-import Contact from "./Contact";
 import RedirectHandler from "./RedirectHandler.js";
+import NotFound from "./NotFound";
+
+const Home = React.lazy(() => import("./Home"));
+const About = React.lazy(() => import("./About"));
+const Services = React.lazy(() => import("./Services"));
+const Portfolio = React.lazy(() => import("./Portfolio"));
+const Contact = React.lazy(() => import("./Contact"));
 
 export default function App() {
   return (
@@ -16,13 +18,16 @@ export default function App() {
       <div className="flex flex-col min-h-screen">
         <RedirectHandler />
         <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </Router>
