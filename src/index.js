05e14a593294f1e6e7ef235ab21f844fa586a93b
jacobadam <jacobanevitt@gmail.com>
2024-12-20 15:48:29 -0800

    fix code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
