062897255973b536007fe454d872e3453136fb12
jacobadam <jacobanevitt@gmail.com>
2024-12-20 16:03:18 -0800

    add back redirect handler

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import NotFound from "./NotFound";
+import RedirectHandler from "./RedirectHandler.js";
+
 const Home = React.lazy(() => import("./Home"));
 const About = React.lazy(() => import("./About"));
 const Services = React.lazy(() => import("./Services"));
@@ -15,6 +17,7 @@ export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
+        <RedirectHandler />
         <Nav />
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
