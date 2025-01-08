f066b3ad8c243aed5d5033e406fdc2850efda226
jacobadam <jacobanevitt@gmail.com>
2025-01-08 10:16:17 -0800

    testing out removing lazy load

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
 import "./App.css";
-import React, { Suspense } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ClipLoader } from "react-spinners";
 
 import Nav from "./Nav";
 import Footer from "./Footer";
 import NotFound from "./NotFound";
-
-const Home = React.lazy(() => import("./Home"));
-const About = React.lazy(() => import("./About"));
-const Services = React.lazy(() => import("./Services"));
-const Portfolio = React.lazy(() => import("./Portfolio"));
-const Contact = React.lazy(() => import("./Contact"));
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
 
 export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Nav />
-        <Suspense
-          fallback={
-            <div className="flex justify-center items-center h-screen">
-              <ClipLoader color="#000000" size={50} />
-            </div>
-          }
-        >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
