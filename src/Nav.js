60edf27320de210d4ad895fb660683a2bef2fcd2
jacobadam <jacobanevitt@gmail.com>
2025-03-05 09:04:34 -0800

    add padding for nav on mobile

--- src/Nav.js
+++ src/Nav.js
@@ -32,7 +32,7 @@ export default function NavBar() {
 
   return (
     <nav
-      className={`sticky top-0 z-10 py-3 w-full transition-colors duration-300 ${
+      className={`sticky top-0 z-10 py-3 px-4 md:px-0 w-full transition-colors duration-300 ${
         isScrolled
           ? "bg-white dark:bg-black shadow-lg"
           : "bg-gradient-to-b from-white to-[#e2e8f0] dark:from-black dark:to-black dark:bg-black"
