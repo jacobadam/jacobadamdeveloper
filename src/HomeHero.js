cc96eeab57f7acc7e8591ecfd2d4c9f7a61d454c
jacobadam <jacobanevitt@gmail.com>
2025-04-10 11:33:11 -0600

    update slogan and add animation, update color on buttons for rebrand

--- src/HomeHero.js
+++ src/HomeHero.js
@@ -1,12 +1,36 @@
+// import { useEffect } from "react";
+import { motion } from "motion/react";
+
 export default function HomepageHero() {
+  const text = "Web Design That Grows With Your Business";
+  const words = text.split(" ");
   return (
     <section className="h-screen sm:pb-20 bg-slate-200 dark:bg-black">
       <div className="h-full flex flex-col lg:flex-row items-center justify-center relative max-w-screen-xl mx-auto overflow-hidden px-4 pb-16 md:pb-0 md:px-8">
-        {/* Text Section */}
         <div className="flex flex-col items-center justify-center px-4 text-center md:max-w-xl mx-auto">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-800 dark:text-white font-extrabold">
-            Custom Websites That Deliver Results
-          </h1>
+          <motion.h1
+            className="flex flex-row flex-wrap justify-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-800 dark:text-white font-extrabold"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            {words.map((word, wordIndex) => (
+              <span key={wordIndex} className="flex flex-row mr-2">
+                {word.split("").map((letter, letterIndex) => (
+                  <motion.span
+                    key={`${wordIndex}-${letterIndex}`}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                      duration: 0.3,
+                      delay: (wordIndex * 5 + letterIndex) / 20,
+                    }}
+                  >
+                    {letter}
+                  </motion.span>
+                ))}
+              </span>
+            ))}
+          </motion.h1>
           <p className="mt-4 text-gray-600 dark:text-white">
             With a focus on clean, responsive design and search engine
             optimization, I build sites that help your business thrive.
@@ -14,7 +38,7 @@ export default function HomepageHero() {
           <div className="flex items-center xxxs:gap-x-4 mt-8 lg:mt-4">
             <a
               href="/services"
-              className="flex items-center justify-center gap-x-1 py-1 xxxs:py-2 px-4 text-white font-medium bg-indigo-600 duration-150 rounded-full"
+              className="flex items-center justify-center gap-x-1 py-1 xxxs:py-2 px-4 text-black dark:text-grey-200 font-medium bg-[#8FBC8F] duration-150 rounded-full"
               aria-label="Go to services page"
             >
               <span>Services</span>
@@ -30,7 +54,7 @@ export default function HomepageHero() {
             </a>
             <a
               href="/contact"
-              className="flex items-center justify-center gap-x-1 py-1 xxxs:py-2 px-4 text-gray-700 xxxs:border-black xxxs:border xxxs:rounded-full duration-150 font-semibold text-nowrap dark:text-white dark:border-indigo-600"
+              className="flex items-center justify-center gap-x-1 py-1 xxxs:py-2 px-4 text-black xxxs:border-black xxxs:border xxxs:rounded-full duration-150 font-semibold text-nowrap dark:text-[#8FBC8F] dark:border-[#8FBC8F]"
               aria-label="Go to contact page"
             >
               <span>Get in touch</span>
