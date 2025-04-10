a6808c767f4014b607b024f7e9ea7379b15618af
jacobadam <jacobanevitt@gmail.com>
2025-04-10 11:50:01 -0600

    update text color

--- src/HomeHero.js
+++ src/HomeHero.js
@@ -5,11 +5,11 @@ export default function HomepageHero() {
   const text = "Web Design That Grows With Your Business";
   const words = text.split(" ");
   return (
-    <section className="h-screen sm:pb-20 bg-slate-200 dark:bg-black">
+    <section className="h-screen sm:pb-20 bg-slate-100 dark:bg-black">
       <div className="h-full flex flex-col lg:flex-row items-center justify-center relative max-w-screen-xl mx-auto overflow-hidden px-4 pb-16 md:pb-0 md:px-8">
         <div className="flex flex-col items-center justify-center px-4 text-center md:max-w-xl mx-auto">
           <motion.h1
-            className="flex flex-row flex-wrap justify-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-800 dark:text-white font-extrabold"
+            className="flex flex-row flex-wrap justify-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-800 dark:text-slate-100 font-extrabold"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
           >
@@ -31,7 +31,7 @@ export default function HomepageHero() {
               </span>
             ))}
           </motion.h1>
-          <p className="mt-4 text-gray-600 dark:text-white">
+          <p className="mt-4 text-gray-600 dark:text-slate-100">
             With a focus on clean, responsive design and search engine
             optimization, I build sites that help your business thrive.
           </p>
