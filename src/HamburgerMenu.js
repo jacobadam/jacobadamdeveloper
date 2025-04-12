0c4969eeab9fa2e2767423d07f9af972814d99fb
jacobadam <jacobanevitt@gmail.com>
2025-04-12 11:51:00 -0600

    add hamburgermenu component

---
+++ src/HamburgerMenu.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { motion, MotionConfig } from "motion/react";
+
+const HamburgerMenu = ({ isMenuOpen, onToggle }) => {
+  return (
+    <MotionConfig
+      transition={{
+        duration: 0.5,
+        ease: "easeInOut",
+      }}
+    >
+      <motion.button
+        initial={false}
+        animate={isMenuOpen ? "open" : "closed"}
+        onClick={onToggle}
+        className="relative w-8 h-8 p-1 justify-center rounded-full bg-white/0 transition-colors hover:bg-red/50 lg:hidden"
+      >
+        <motion.span
+          variants={VARIANTS.top}
+          className="absolute h-1 w-8 bg-black dark:bg-slate-100"
+          style={{ y: "-50%", left: "50%", x: "-50%", top: "35%" }}
+        />
+        <motion.span
+          variants={VARIANTS.middle}
+          className="absolute h-1 w-8 bg-black dark:bg-slate-100"
+          style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
+        />
+        <motion.span
+          variants={VARIANTS.bottom}
+          className="absolute h-1 w-5 bg-black dark:bg-slate-100"
+          style={{
+            x: "-50%",
+            y: "50%",
+            bottom: "35%",
+            left: "calc(50% + 6px)",
+          }}
+        />
+      </motion.button>
+    </MotionConfig>
+  );
+};
+
+const VARIANTS = {
+  top: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      top: ["35%", "50%", "50%"],
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      top: ["50%", "50%", "25%"],
+    },
+  },
+  middle: {
+    open: {
+      rotate: ["0deg", "0deg", "-45deg"],
+    },
+    closed: {
+      rotate: ["-45deg", "0deg", "0deg"],
+    },
+  },
+  bottom: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      bottom: ["35%", "50%", "50%"],
+      left: "50%",
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      bottom: ["50%", "50%", "25%"],
+      left: "calc(50% + 6px)",
+    },
+  },
+};
+
+export default HamburgerMenu;
