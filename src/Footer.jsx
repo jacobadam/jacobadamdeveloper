29aff6da9acbe8620bd7128ce3d1c0d9ea8f495d
jacobadam <jacobanevitt@gmail.com>
2025-04-19 13:01:31 -0600

    update copyright and remove linkedin

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,24 +6,8 @@ export default function Footer() {
       <div className="w-full">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="max-w-3xl mx-auto">
-            <div className="flex space-x-10 justify-center items-center mb-10 mt-4">
-              <a
-                href="https://www.linkedin.com/in/jacobnevitt/"
-                className="block text-gray-900 dark:invert-0 invert"
-              >
-                <div className="flex justify-center items-center">
-                  <img
-                    src="/linkedin.svg"
-                    alt="LinkedIn Logo"
-                    className="w-8 h-8 object-contain"
-                    width="32"
-                    height="32"
-                  />
-                </div>
-              </a>
-            </div>
             <span className="text-lg text-gray-500 text-center block pb-4">
-              © 2025 Jacob Adam. All Rights Reserved.
+              © 2025 Greenhouse Web Designs. All Rights Reserved.
             </span>
           </div>
         </div>
