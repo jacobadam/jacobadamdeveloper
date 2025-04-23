203b74eb9ef77d7f2b7da0b150799e68dd1ced43
jacobadam <jacobanevitt@gmail.com>
2025-04-23 12:43:05 -0600

    update home project style for dark mode

--- src/HomeProjects.jsx
+++ src/HomeProjects.jsx
@@ -2,31 +2,36 @@ import React from "react";
 
 export default function HomeProjects() {
   return (
-    <section className="py-20 relative bg-slate-100 dark:bg-black">
+    <section className="w-full h-full py-40 relative bg-slate-100 dark:bg-zinc-950">
       <div className="w-full max-w-2xl lg:max-w-7xl px-6 lg:px-8 mx-auto">
-        <h1 className="font-manrope font-medium text-4xl text-gray-900 dark:text-white mb-10 max-md:text-center">
+        <h1 className="text-2xl xl:text-3xl font-bold text-gray-900 dark:text-gray-50 mb-16 max-md:text-center">
           Professional Project Achievements
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-          <div className="group flex items-center flex-col gap-8 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+          <a
+            href="https://www.katieshevlin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group flex items-center flex-col gap-8 w-full cursor-pointer"
+          >
             <div className="w-full h-60">
               <img
-                className="w-full h-full rounded-3xl object-cover"
+                className="w-full h-full rounded-3xl object-cover transform transition-transform duration-300 group-hover:scale-105"
                 src="https://www.katieshevlin.com/hero-image.webp"
                 alt="Content services website"
               />
             </div>
             <div className="flex items-center justify-between max-w-[406px] lg:max-w-full w-full lg:px-0">
               <div className="block">
-                <h4 className="text-2xl font-manrope font-semibold text-gray-900 dark:text-white mb-1">
+                <h4 className="text-2xl font-semibold text-gray-900 dark:text-gray-50 mb-1">
                   Content Services Website
                 </h4>
                 <p className="font-medium text-lg text-gray-400">
                   Content Design, Marketing
                 </p>
               </div>
-              <button className="border border-black bg-[#8FBC8F] hover:border-[#8FBC8F] py-2 px-3.5 rounded-full transition-all duration-300 group-hover:bg-black">
+              <button className="border border-black dark:group-hover:border-white bg-[#8FBC8F] py-2 px-4 rounded-full transition-all duration-300 group-hover:bg-[#8FBC8F]">
                 <svg
                   className="stroke-black transition-all duration-300 group-hover:stroke-white"
                   xmlns="http://www.w3.org/2000/svg"
@@ -44,24 +49,29 @@ export default function HomeProjects() {
                 </svg>
               </button>
             </div>
-          </div>
+          </a>
 
-          <div className="group flex items-center flex-col gap-8 w-full">
+          <a
+            href="https://www.neilnevitt.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group flex items-center flex-col gap-8 w-full cursor-pointer"
+          >
             <div className="w-full h-60">
               <img
-                className="w-full h-full rounded-3xl object-cover"
+                className="w-full h-full rounded-3xl object-cover transform transition-transform duration-300 group-hover:scale-105"
                 src="https://www.neilnevitt.com/homepage-image.webp"
                 alt="Healthcare Website"
               />
             </div>
             <div className="flex items-center justify-between max-w-[406px] lg:max-w-full w-full lg:px-0">
               <div className="block">
-                <h4 className="text-2xl font-manrope font-semibold text-gray-900 dark:text-white mb-1">
+                <h4 className="text-2xl font-semibold text-gray-900 dark:text-gray-50 mb-1">
                   Healthcare Website
                 </h4>
                 <p className="font-medium text-lg text-gray-400">Healthcare</p>
               </div>
-              <button className="border border-black bg-[#8FBC8F] hover:border-[#8FBC8F] py-2 px-3.5 rounded-full transition-all duration-300 group-hover:bg-black">
+              <button className="border border-black dark:group-hover:border-white bg-[#8FBC8F] py-2 px-4 rounded-full transition-all duration-300 group-hover:bg-[#8FBC8F]">
                 <svg
                   className="stroke-black transition-all duration-300 group-hover:stroke-white"
                   xmlns="http://www.w3.org/2000/svg"
@@ -79,7 +89,7 @@ export default function HomeProjects() {
                 </svg>
               </button>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </section>
