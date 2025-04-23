508ec7c1ca57a525aa943922ec120d7efd992403
jacobadam <jacobanevitt@gmail.com>
2025-04-23 10:58:43 -0600

    update home to use parallax scroll effect

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,57 @@
+import HomeHero from "./HomeHero.jsx";
+import HomeServices from "./HomeServices.jsx";
 import HomeBenefits from "./HomeBenefits.jsx";
 import HomeContact from "./HomeContact.jsx";
-import HomeHero from "./HomeHero.jsx";
 import HomeProjects from "./HomeProjects.jsx";
-import HomeServices from "./HomeServices.jsx";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "motion/react";
 
 export default function Home() {
   return (
     <div>
-      <HomeHero />
-      <HomeServices />
-      <HomeBenefits />
-      <HomeProjects />
-      <HomeContact />
+      <ParallaxComponent>
+        <HomeHero />
+      </ParallaxComponent>
+      <ParallaxComponent>
+        <HomeServices />
+      </ParallaxComponent>
+      <ParallaxComponent>
+        <HomeBenefits />
+      </ParallaxComponent>
+      <ParallaxComponent>
+        <HomeProjects />
+      </ParallaxComponent>
+      <ParallaxComponent>
+        <HomeContact />
+      </ParallaxComponent>
     </div>
   );
 }
+
+const ParallaxComponent = ({ children }) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["end end", "end start"],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 1]);
+  const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [1, 0, 0]);
+
+  return (
+    <div className="">
+      <motion.div
+        ref={targetRef}
+        style={{
+          scale,
+          opacity,
+
+          top: 0,
+        }}
+        className="sticky z-0 overflow-hidden flex items-center justify-center"
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+};
