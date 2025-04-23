232f7bc73414f5881fb564790bc5d3a0bde191e9
jacobadam <jacobanevitt@gmail.com>
2025-04-23 13:46:19 -0600

    add z-index to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   const companyName = "Greenhouse Web Designs";
 
   return (
-    <footer className="w-full bg-transparent min-h-[80px] py-4 flex items-center justify-center">
+    <footer className="w-full bg-transparent min-h-[80px] py-4 flex items-center justify-center z-10">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto text-center">
           <span className="text-sm text-gray-500 block">
