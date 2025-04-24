02cc4fa24010ce78511ebc73417b86351e980fe1
jacobadam <jacobanevitt@gmail.com>
2025-04-24 10:41:45 -0600

    update home service for light

--- src/HomeServices.jsx
+++ src/HomeServices.jsx
@@ -34,21 +34,21 @@ const services = [
 
 export default function HomeServices() {
   return (
-    <section className="w-full h-full py-24 md:py-0 flex items-center justify-center bg-[#002366] dark:bg-zinc-950">
+    <section className="w-full h-full py-24 md:py-8 flex items-center justify-center bg-zinc-950 dark:bg-zinc-950">
       <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
         <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
           <div className="lg:w-4/5 flex flex-col items-center justify-center mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
-            <h1 className="text-2xl xl:text-3xl font-bold mb-2 dark:text-gray-50">
+            <h1 className="text-2xl xl:text-3xl font-bold mb-2 text-zinc-100">
               Quality Design, Real Impact
             </h1>
-            <p className="text-gray-600 dark:text-gray-300 text-center leading-relaxed">
+            <p className="text-gray-300 text-center leading-relaxed">
               Discover a range of solutions tailored <br /> to meet your
               business needs
             </p>
             <a
               href="/services"
               aria-label="Go to services"
-              className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border border-[#6fa96f] bg-gray-50 dark:bg-transparent px-3 text-black dark:text-[#6fa96f] dark:hover:text-black hover:shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-[#6fa96f] before:transition-all before:duration-500 hover:text-white hover:shadow-[#6fa96f] hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
+              className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border border-[#6fa96f] bg-transparent px-3 text-[#6fa96f] dark:hover:text-black hover:shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-[#6fa96f] before:transition-all before:duration-500 hover:text-white hover:shadow-[#6fa96f] hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
             >
               <span className="relative">Services</span>
             </a>
@@ -63,7 +63,7 @@ export default function HomeServices() {
                 <div className="h-full relative flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-slate-800 dark:bg-black p-8 transition-colors duration-500 group-hover:bg-slate-800">
                   <div className="inline-block rounded-lg p-2">
                     <img
-                      className="w-11 h-11 object-contain bg-gray-50 p-1 rounded"
+                      className="w-11 h-11 object-contain bg-white p-1 rounded"
                       key={index}
                       src={service.svgPath}
                       alt={service.alt}
