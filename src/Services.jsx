2430a4aa8692567b5f89307b719e786ba489498c
jacobadam <jacobanevitt@gmail.com>
2025-04-25 13:05:15 -0600

    add padding to service page

--- src/Services.jsx
+++ src/Services.jsx
@@ -8,7 +8,13 @@ export default function Services() {
   };
 
   return (
-    <section className="pt-4 xs:pt-8 pb-20 relative bg-transparent flex-grow">
+    <section
+      className="pt-4 sm:pt-8 pb-20 relative bg-transparent flex-grow"
+      style={{
+        paddingTop: "clamp(10rem, 10vw, 8rem)",
+        minHeight: "calc(100vh - clamp(4rem, 10vw, 8rem))",
+      }}
+    >
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mb-4 lg:mb-8 text-center">
           <div className="flex items-center justify-between gap-5 flex-col mb-14">
